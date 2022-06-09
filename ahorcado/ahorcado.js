192d6132242192beedc1277d0b7e6c732826e1f2
Marisol Rossow <76127169+marisolrossow@users.noreply.github.com>
2022-06-09 01:00:37 -0300

    Add files via upload

---
+++ ahorcado/ahorcado.js
@@ -0,0 +1,54 @@
+// crea el arreglo de palabras secretas
+var palabras = ["JAVASCRIPT", "HTML", "ALURA", "ORACLE", "CSS", "PROGRAMACION", "FRONTEND", "BACKEND", "LOGICA"]
+// creacion de variables
+var palNueva;
+var letra;
+var letrasRepetidas= [];
+var letrasErroneas= [];
+var vidasTotales = 5
+var palabra;
+var palabraGuion;
+var repetida = false
+
+
+
+
+
+//oculto canvas
+document.getElementById("ahorcado").style.display = "none";
+//oculto boton reinicio
+document.getElementById("reiniciar-juego").style.display= "none";
+
+
+// captura el boton de iniciar juego
+var jugar = document.getElementById("iniciar-juego")
+jugar.addEventListener("click", function(){
+    // hago visible el juego
+    document.getElementById("areaJuego").style.display = "flex";
+   
+    //muestro el canvas
+    
+    document.getElementById("ahorcado").style.display = "block";
+    
+    // selecciono una palabra a adivinar por el usuario
+    palabra = palabras[Math.floor(Math.random() * palabras.length)]
+
+    // convertir la palabra seleccionada a guiones bajos
+    palabraGuion = palabra.replace(/./g , "_ ")
+
+    // hacerlo visible la palabra seleccionada con guiones
+    document.getElementById("palabraSecreta").innerHTML = palabraGuion
+    //muestra el titulo de las letras erradas
+    document.getElementById("tituloLetras").innerHTML = "Letras equivocadas"
+
+    // se muestra la cantidad de vidas
+    document.getElementById("vidas").innerHTML = "La cantidad de vidas son:  " + vidasTotales; 
+    //llama a la funcion de dibujar la base del ahorcado
+    dibujarBase()
+    // captura el teclado
+    
+    window.addEventListener("keydown", ingresarLetra);
+
+    
+    
+})
---
+++ ahorcado/canvas.js
@@ -0,0 +1,108 @@
+var pantalla = document.querySelector("#ahorcado")
+
+var pincel = pantalla.getContext("2d")
+pincel.fillStyle= "grey"
+
+function dibujarBase(){
+    pincel.beginPath();
+    pincel.moveTo(20, 350);
+    
+    pincel.lineTo(300, 350);
+    
+    pincel.lineTo(500,350)
+    
+    pincel.lineWidth = 15;
+    pincel.strokeStyle = "#000"
+    pincel.stroke()
+    pincel.closePath();
+
+
+}
+
+function dibujarPoste(){
+    
+    pincel.beginPath();
+    pincel.moveTo(150, 100);
+    pincel.lineTo(150, 100);
+    pincel.moveTo(150, 120);
+    pincel.lineTo(150, 100);
+    pincel.lineTo(50,100);    
+    pincel.lineTo(50, 350);
+    pincel.lineWidth = 3;
+    pincel.strokeStyle = "#000"
+    pincel.stroke()
+    pincel.closePath();
+
+}
+
+
+
+
+function dibujarRostro(){
+    pincel.beginPath();
+    pincel.arc(150,150,35,0,Math.PI * 2, false);
+    pincel.strokeStyle = "navy";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath();
+
+
+}
+
+function dibujarTorzo(){
+    pincel.beginPath();
+    pincel.moveTo(150,180);
+    pincel.lineTo(150,250);
+    
+    pincel.strokeStyle = "navy";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarBrazos(){
+    pincel.beginPath();
+    pincel.moveTo(120,220);
+    pincel.lineTo(150,180);
+    pincel.lineTo(180,220)
+    pincel.strokeStyle = "navy";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarPiernas(){
+    pincel.beginPath();
+    pincel.moveTo(120,290);
+    pincel.lineTo(150,250);
+    pincel.lineTo(180,290)
+    pincel.strokeStyle = "navy";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarOjosMuertos(){
+    pincel.beginPath();
+    
+    //ojo izquierdo
+    pincel.moveTo(125,120);
+    pincel.lineTo(145,145);
+    pincel.moveTo(145,120);
+    pincel.lineTo(125,145);
+    
+    
+    //ojo derecho
+    pincel.moveTo(155,120);
+    pincel.lineTo(175,145);
+    pincel.moveTo(175,120);
+    pincel.lineTo(155,145);
+
+    pincel.strokeStyle = "navy";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+}
